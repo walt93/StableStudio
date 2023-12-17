db64f3f8ad0b16990f0b9fc13784310f662ab71b
Conspyre <admin@conspyre.tv>
2023-12-16 23:32:41 -0800

    remove shortcuts menu

--- packages/stablestudio-ui/src/App/TopBar/Right/index.tsx
+++ packages/stablestudio-ui/src/App/TopBar/Right/index.tsx
@@ -8,20 +8,6 @@ export const Right = () => {
   return (
     <div className="flex grow basis-0 items-center justify-end gap-2">
       <>
-        {!isMobileDevice && (
-          <Theme.Button
-            outline
-            className="rounded-full"
-            icon={Theme.Icon.Keyboard}
-            onClick={() =>
-              // cause a Ctrl+K to be triggered
-              setIsOpen(true)
-            }
-          >
-            Shortcuts
-            <Shortcut.Keys keys={["Meta", "k"]} className="ml-2" />
-          </Theme.Button>
-        )}
         <Link to="/settings">
           <Theme.Button
             outline
