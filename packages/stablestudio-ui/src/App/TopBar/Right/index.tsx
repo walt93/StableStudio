c1feab016c86adf6a999f3984a8aca4e44e80832
Conspyre <admin@conspyre.tv>
2023-12-16 23:37:33 -0800

    comment it out another way

--- packages/stablestudio-ui/src/App/TopBar/Right/index.tsx
+++ packages/stablestudio-ui/src/App/TopBar/Right/index.tsx
@@ -8,21 +8,21 @@ export const Right = () => {
   return (
     <div className="flex grow basis-0 items-center justify-end gap-2">
       <>
-        {!isMobileDevice && (
-          // <Theme.Button
-          //   outline
-          //   className="rounded-full"
-          //   icon={Theme.Icon.Keyboard}
-          //   onClick={() =>
-          //     // cause a Ctrl+K to be triggered
-          //     setIsOpen(true)
-          //   }
-          // >
-          //   Shortcuts
-          //   <Shortcut.Keys keys={["Meta", "k"]} className="ml-2" />
-          // </Theme.Button>
+{/*        {!isMobileDevice && (
+          <Theme.Button
+            outline
+            className="rounded-full"
+            icon={Theme.Icon.Keyboard}
+            onClick={() =>
+              // cause a Ctrl+K to be triggered
+              setIsOpen(true)
+            }
+          >
+            Shortcuts
+            <Shortcut.Keys keys={["Meta", "k"]} className="ml-2" />
+          </Theme.Button>
         )}
-        <Link to="/settings">
+*/}        <Link to="/settings">
           <Theme.Button
             outline
             label="Settings"
